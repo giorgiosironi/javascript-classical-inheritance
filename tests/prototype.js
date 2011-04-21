1ebb7a7475e5545cdbf5c491178b31ba42a3356c
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-04-21 10:36:42 +0200

    Transformed anonymous constructors in named ones

--- tests/prototype.js
+++ tests/prototype.js
@@ -4,47 +4,47 @@ TestCase("playing with object prototypes in JavaScript", {
         assertEquals(Object.prototype.toString, anObject.toString);
     },
     "test constructors have public prototype property" : function () {
-        var constructor = function MyClass() {};
-        assertNotUndefined(constructor.prototype);
+        function MyClass() {};
+        assertNotUndefined(MyClass.prototype);
     },
     "test objects do not have public prototype property" : function () {
         var anObject = {};
         assertUndefined(anObject.prototype);
     },
     "test objects created with a constructor has constructor property" : function () {
-        var constructor = function MyClass() {};
-        var anObject = new constructor();
-        assertTrue(anObject instanceof constructor);
-        assertEquals(constructor, anObject.constructor);
+        function MyClass() {};
+        var anObject = new MyClass();
+        assertTrue(anObject instanceof MyClass);
+        assertEquals(MyClass, anObject.constructor);
     },
     "test objects created with a constructor inherit from Object" : function () {
-        var constructor = function MyClass() {};
-        var anObject = new constructor();
+        function MyClass() {};
+        var anObject = new MyClass();
         assertEquals(Object.prototype.toString, anObject.toString);
     },
     "test the prototype chain goes from instance to constructor" : function() {
-        var constructor = function MyClass() {};
-        var anObject = new constructor();
-        constructor.prototype.doSomething = function () {};
-        assertEquals(constructor.prototype.doSomething, anObject.doSomething);
+        function MyClass() {};
+        var anObject = new MyClass();
+        MyClass.prototype.doSomething = function () {};
+        assertEquals(MyClass.prototype.doSomething, anObject.doSomething);
     },
     "test the prototype chain goes also to Object" : function () {
-        var constructor = function MyClass() {};
-        var anObject = new constructor();
+        function MyClass() {};
+        var anObject = new MyClass();
         Object.prototype.doSomethingElse = function () {};
         assertEquals(Object.prototype.doSomethingElse, anObject.doSomethingElse);
     },
     "test inheritance can be built by substituing prototypes with an object of the parent class" : function () {
-        var animal = function Animal() {};
-        animal.prototype.eat = function() { return 'Yum'; };
-        var dog = function Dog() {};
-        dog.prototype = new animal();
-        dog.prototype.bark = function() { return 'Arf'; };
-        dog.prototype.constructor = dog;
-        var lassie = new dog();
-        assertTrue(lassie instanceof dog);
+        function Animal() {};
+        Animal.prototype.eat = function() { return 'Yum'; };
+        function Dog() {};
+        Dog.prototype = new Animal();
+        Dog.prototype.bark = function() { return 'Arf'; };
+        Dog.prototype.constructor = Dog;
+        var lassie = new Dog();
+        assertTrue(lassie instanceof Dog);
         assertEquals('Arf', lassie.bark());
-        assertTrue(lassie instanceof animal);
+        assertTrue(lassie instanceof Animal);
         assertEquals('Yum', lassie.eat());
     }
 });
